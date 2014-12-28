d9d55f1fb544f31bc9ffd354fe05cd9905a942dd
Michele Balistreri <michele@briksoftware.com>
2014-12-28 11:24:02 +0100

    added tests

--- lib/plug_auth/authentication/basic.ex
+++ lib/plug_auth/authentication/basic.ex
@@ -1,8 +1,19 @@
 defmodule PlugAuth.Authentication.Basic do
+  @moduledoc """
+    Implements basic HTTP authentication. To use add
+
+    plug PlugAuth.Authentication.Basic, realm: "Secret world"
+
+    to your pipeline. This module is derived from https://github.com/lexmag/blaguth
+  """ 
+
   @behaviour Plug
   import Plug.Conn
   import PlugAuth.Authentication.Utils
 
+  @doc """
+    Add the credentials for a `user` and `password` combination. `user_data` can be any term but must not be `nil`.
+  """
   def add_credentials(user, password, user_data \\ []) do
     encode_creds(user, password) |> PlugAuth.CredentialStore.put_credentials(user_data)
   end
@@ -23,15 +34,15 @@ defmodule PlugAuth.Authentication.Basic do
 
   defp get_auth_header(conn), do: {conn, get_req_header(conn, "authorization")}
 
-  defp decode_creds({conn, ["Basic " <> encoded_creds | _]}), do: {conn, PlugAuth.CredentialStore.get_user_data(encoded_creds)}
+  defp decode_creds({conn, ["Basic " <> creds | _]}), do: {conn, PlugAuth.CredentialStore.get_user_data(creds)}
   defp decode_creds({conn, _}), do: {conn, nil}
 
   defp assert_creds({conn, nil}, realm), do: halt_with_login(conn, realm)
   defp assert_creds({conn, user_data}, _), do: assign_user_data(conn, user_data)
 
-  def halt_with_login(conn, realm) do
+  defp halt_with_login(conn, realm) do
     conn 
-    |> put_resp_header("Www-Authenticate", ~s[Basic realm="#{realm}"])
+    |> put_resp_header("Www-Authenticate", ~s{Basic realm="#{realm}"})
     |> halt_with_error("HTTP Basic: Access denied.\n")
   end
 end
---
+++ test/plug_auth/authentication/basic_test.exs
@@ -0,0 +1,69 @@
+defmodule PlugAuth.Authentication.Basic.Test do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  defmodule TestPlug do
+    use Plug.Builder
+    import Plug.Conn
+
+    plug PlugAuth.Authentication.Basic, realm: "Secret"
+    plug :index
+
+    defp index(conn, _opts), do: send_resp(conn, 200, "Authorized")
+  end
+
+  defp call(plug, headers) do
+    conn(:get, "/", [], headers: headers)
+    |> plug.call([])
+  end
+
+  defp assert_unauthorized(conn, realm) do
+    assert conn.status == 401
+    assert get_resp_header(conn, "Www-Authenticate") == [~s{Basic realm="#{realm}"}]
+    refute conn.assigns[:authenticated_user]
+  end
+
+  defp assert_authorized(conn, content) do
+    assert conn.status == 200
+    assert conn.resp_body == content
+    assert conn.assigns[:authenticated_user] == %{role: :admin}
+  end
+
+  defp auth_header(creds) do
+    {"authorization", "Basic " <> Base.encode64(creds)}
+  end
+
+  setup do
+    PlugAuth.Authentication.Basic.add_credentials("Admin", "SecretPass", %{role: :admin})
+  end
+
+  test "request without credentials" do
+    conn = call(TestPlug, [])
+    assert_unauthorized conn, "Secret"
+  end
+
+  test "request with invalid user" do
+    conn = call(TestPlug, [auth_header("Hacker:SecretPass")])
+    assert_unauthorized conn, "Secret"
+  end
+
+  test "request with invalid password" do
+    conn = call(TestPlug, [auth_header("Admin:ASecretPass")])
+    assert_unauthorized conn, "Secret"
+  end
+
+  test "request with valid credentials" do
+    conn = call(TestPlug, [auth_header("Admin:SecretPass")])
+    assert_authorized conn, "Authorized"
+  end
+
+  test "request with malformed credentials" do
+    conn = call(TestPlug, [{"authorization", "Basic Zm9)"}])
+    assert_unauthorized conn, "Secret"
+  end
+
+  test "request with wrong scheme" do
+    conn = call(TestPlug, [{"authorization", "Bearer " <> Base.encode64("Admin:SecretPass")}])
+    assert_unauthorized conn, "Secret"
+  end
+end
