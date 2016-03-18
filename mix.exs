e7bf8baaf3a4a69355f0362aa06f2ecbfb198ad0
Michele Balistreri <opensource@bitgamma.com>
2016-03-18 12:55:45 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugAuth.Mixfile do
   def project do
     [
       app: :plug_auth,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.1",
       deps: deps,
       package: package,
